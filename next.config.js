ec9872d32e3f320c1ca5915e7014169554e8f86a
thekiehlb <thekiehlb@example.com>
2022-01-03 09:34:30 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: {
-    reactRoot: true,
-    concurrentFeatures: true,
-    serverComponents: true,
-    swcMinify: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
-
-  swcMinify: false,
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['@svgr/webpack'],
-  //   });
-
-  //   return config;
-  // },
 };
