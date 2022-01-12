39e3e4b1fa342edbe8c9c885db08f7a33edd7f52
kiehlb <thekiehlb@gmail.com>
2022-01-13 06:55:39 +0900

    update

--- src/components/Write/colorBlockPlugin.tsx
+++ src/components/Write/colorBlockPlugin.tsx
@@ -3,24 +3,38 @@ import React from 'react';
 const ColorBlock = React.forwardRef(
   (
     {
+      /* @ts-ignore */
       block, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       blockProps, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       customStyleMap, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       customStyleFn, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       decorator, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       forceSelection, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       offsetKey, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       selection, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       tree, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       contentState, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       blockStyleFn, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       preventScroll, // eslint-disable-line no-unused-vars
+      /* @ts-ignore */
       style,
       ...elementProps
     },
     ref,
   ) => (
     <div
+      /* @ts-ignore */
       ref={ref}
       {...elementProps}
       style={{ width: 200, height: 80, backgroundColor: '#9bc0c7', ...style }}
@@ -29,6 +43,7 @@ const ColorBlock = React.forwardRef(
 );
 
 const createColorBlockPlugin = (config = {}) => {
+  /* @ts-ignore */
   const component = config.decorator ? config.decorator(ColorBlock) : ColorBlock;
   return {
     blockRendererFn: (block, { getEditorState }) => {
