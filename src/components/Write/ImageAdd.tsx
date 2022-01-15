d2293071070f32308bc1fb2cb316a6c5b29f5fb4
kiehlb <thekiehlb@gmail.com>
2022-01-16 00:04:46 +0900

    update

--- src/components/Write/ImageAdd.tsx
+++ src/components/Write/ImageAdd.tsx
@@ -105,7 +105,7 @@ export default class ImageAdd extends Component {
             <svg
               stroke="currentColor"
               fill="currentColor"
-              stroke-width="0"
+              strokeWidth="0"
               viewBox="0 0 24 24"
               height="1.5rem"
               width="1.5rem"
