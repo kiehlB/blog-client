8dd465abca714bac7ef5fe284a3a6d50d766ec10
kiehlb <thekiehlb@gmail.com>
2022-01-20 11:05:30 +0900

    update

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -11,7 +11,7 @@ let apolloClient;
 
 //  prod ? process.env.API_URL : 'http://localhost:4000/graphql',
 const link = new HttpLink({
-  uri: 'https://api.woongblog.shop/graphql',
+  uri: 'https://api.woongblog.xyz/graphql',
   credentials: 'include',
 });
 
