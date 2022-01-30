93d54614bb5f583d7147b111f315bedc795cc5c8
kiehlb <thekiehlb@gmail.com>
2022-01-30 19:23:29 +0900

    update

--- src/components/Write/ImageAdd.tsx
+++ src/components/Write/ImageAdd.tsx
@@ -1,169 +1,9 @@
-// import React, { Component } from 'react';
-// import styles from './styles.module.css';
-// import imageCompression from 'browser-image-compression';
-// import styled from 'styled-components';
-// import { Spinner } from 'evergreen-ui';
-// import { Pane, Badge, Text } from 'evergreen-ui';
-
-// export default class ImageAdd extends Component {
-//   // Start the popover closed
-//   state = {
-//     open: false,
-//     fileInputState: '',
-//     selectedFile: '',
-//     waitForImg: 0,
-//   };
-
-//   // When the popover is open and users click anywhere on the page,
-//   // the popover should close
-//   componentDidMount() {
-//     document.addEventListener('click', this.closePopover);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('click', this.closePopover);
-//   }
-
-//   // Note: make sure whenever a click happens within the popover it is not closed
-//   onPopoverClick = () => {
-//     // @ts-ignore
-//     this.preventNextClose = true;
-//   };
-
-//   openPopover = () => {
-//     if (!this.state.open) {
-//       // @ts-ignore
-//       this.preventNextClose = true;
-//       this.setState({
-//         open: true,
-//       });
-//     }
-//   };
-
-//   closePopover = () => {
-//     // @ts-ignore
-//     if (!this.preventNextClose && this.state.open) {
-//       this.setState({
-//         open: false,
-//       });
-//     }
-
-//     // @ts-ignore
-//     this.preventNextClose = false;
-//   };
-
-//   addImage = base64EncodedImag => {
-//     // @ts-ignore
-
-//     const { editorState, onChange } = this.props;
-
-//     // @ts-ignore
-//     onChange(this.props.modifier(editorState, base64EncodedImag));
-//     this.setState({ waitForImg: 2 });
-//   };
-
-//   changeUrl = evt => {
-//     this.setState({ fileInputState: evt.target.value });
-//   };
-
-//   handleFileInputChange = async e => {
-//     e.preventDefault();
-//     const file = e.target.files[0];
-//     this.setState({ waitForImg: 1 });
-
-//     const options = {
-//       maxSizeMB: 0.1,
-//       maxWidthOrHeight: 1024,
-//       useWebWorker: true,
-//     };
-
-//     try {
-//       const compressedFile = await imageCompression(file, options);
-//       console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
-//       console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
-
-//       this.setState({ selectedFile: compressedFile });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     if (!this.state.selectedFile) return;
-//     const reader = new FileReader();
-//     // @ts-ignore
-//     reader.readAsDataURL(this.state.selectedFile);
-//     reader.onloadend = () => {
-//       this.addImage(reader.result);
-//     };
-//   };
-
-//   WaitingFotImg = readyForFile => {
-//     if (readyForFile == 0) {
-//       return (
-//         <>
-//           <div></div>
-//         </>
-//       );
-//     } else if (readyForFile == 1) {
-//       return (
-//         <>
-//           <Spinner size={16} />
-//         </>
-//       );
-//     } else if (readyForFile == 2) {
-//       return (
-//         <>
-//           <Pane flexBasis={120}>
-//             <Badge color="green">Success</Badge>
-//           </Pane>
-//         </>
-//       );
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <ButtonStyles>
-//           <div className="flex">
-//             <label htmlFor="fileInput">
-//               <svg
-//                 stroke="currentColor"
-//                 fill="currentColor"
-//                 strokeWidth="0"
-//                 viewBox="0 0 24 24"
-//                 height="1.5rem"
-//                 width="1.5rem"
-//                 xmlns="http://www.w3.org/2000/svg">
-//                 <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"></path>
-//               </svg>
-//             </label>
-
-//             <input
-//               id="fileInput"
-//               name="image"
-//               type="file"
-//               className="p-1.5 cursor-pointer hover:bg-neutral-100 transition-all"
-//               onChange={this.handleFileInputChange}
-//               value={this.state.fileInputState}
-//               style={{ display: 'none' }}
-//             />
-//             <div className="ml-2">{this.WaitingFotImg(this.state.waitForImg)}</div>
-//           </div>
-//         </ButtonStyles>
-//       </>
-//     );
-//   }
-// }
-
-// const ButtonStyles = styled.div`
-//   font-size: 1rem;
-//   font-weight: bold;
-//   color: rgb(134, 142, 150);
-//   cursor: pointer;
-// `;
-
 import React, { Component } from 'react';
 import styles from './styles.module.css';
 import imageCompression from 'browser-image-compression';
+import styled from 'styled-components';
+import { Spinner } from 'evergreen-ui';
+import { Pane, Badge, Text } from 'evergreen-ui';
 
 export default class ImageAdd extends Component {
   // Start the popover closed
@@ -171,6 +11,7 @@ export default class ImageAdd extends Component {
     open: false,
     fileInputState: '',
     selectedFile: '',
+    waitForImg: 0,
   };
 
   // When the popover is open and users click anywhere on the page,
@@ -213,9 +54,12 @@ export default class ImageAdd extends Component {
 
   addImage = base64EncodedImag => {
     // @ts-ignore
+
     const { editorState, onChange } = this.props;
+
     // @ts-ignore
     onChange(this.props.modifier(editorState, base64EncodedImag));
+    this.setState({ waitForImg: 2 });
   };
 
   changeUrl = evt => {
@@ -225,16 +69,19 @@ export default class ImageAdd extends Component {
   handleFileInputChange = async e => {
     e.preventDefault();
     const file = e.target.files[0];
+    this.setState({ waitForImg: 1 });
 
     const options = {
-      maxSizeMB: 0.1,
-      maxWidthOrHeight: 1920,
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1024,
       useWebWorker: true,
     };
 
     try {
-      console.log(e.target.value);
       const compressedFile = await imageCompression(file, options);
+      console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+      console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+
       this.setState({ selectedFile: compressedFile });
     } catch (error) {
       console.log(error);
@@ -248,35 +95,68 @@ export default class ImageAdd extends Component {
     };
   };
 
-  handleSubmitFile = e => {
-    e.preventDefault();
+  WaitingFotImg = readyForFile => {
+    if (readyForFile == 0) {
+      return (
+        <>
+          <div></div>
+        </>
+      );
+    } else if (readyForFile == 1) {
+      return (
+        <>
+          <Spinner size={16} />
+        </>
+      );
+    } else if (readyForFile == 2) {
+      return (
+        <>
+          <Pane flexBasis={120}>
+            <Badge color="green">Success</Badge>
+          </Pane>
+        </>
+      );
+    }
   };
 
   render() {
-    const popoverClassName = this.state.open
-      ? styles.addImagePopover
-      : styles.addImageClosedPopover;
-    const buttonClassName = this.state.open
-      ? styles.addImagePressedButton
-      : styles.addImageButton;
-
     return (
       <>
-        <div className={styles.addImage}>
-          <button className={buttonClassName} onMouseUp={this.openPopover} type="button">
-            +
-          </button>
-          <div className={popoverClassName} onClick={this.onPopoverClick}>
+        <ButtonStyles>
+          <div className="flex">
+            <label htmlFor="fileInput">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 24 24"
+                height="1.5rem"
+                width="1.5rem"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"></path>
+              </svg>
+            </label>
+
             <input
               id="fileInput"
-              type="file"
               name="image"
+              type="file"
+              className="p-1.5 cursor-pointer hover:bg-neutral-100 transition-all"
               onChange={this.handleFileInputChange}
               value={this.state.fileInputState}
+              style={{ display: 'none' }}
             />
+            <div className="ml-2">{this.WaitingFotImg(this.state.waitForImg)}</div>
           </div>
-        </div>
+        </ButtonStyles>
       </>
     );
   }
 }
+
+const ButtonStyles = styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+  color: rgb(134, 142, 150);
+  cursor: pointer;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,155 +43,155 @@ const Home: NextPage = (props: any) => {
   } = useGetPosts();
 
   //canvas
-  const canvasRef = useRef(null);
-  function Circle(xCoordinate, yCoordinate, radius, ctx, mouseX, mouseY) {
-    const colorArray = ['#272F32', '#9DBDC6', '#FF3D2E', '#DAEAEF'];
-    // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // ctx.fillStyle = '#000000';
-    // ctx.beginPath();
-    // ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    // ctx.fill();
-    const maxRadius = 35;
+  // const canvasRef = useRef(null);
+  // function Circle(xCoordinate, yCoordinate, radius, ctx, mouseX, mouseY) {
+  //   const colorArray = ['#272F32', '#9DBDC6', '#FF3D2E', '#DAEAEF'];
+  //   // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  //   // ctx.fillStyle = '#000000';
+  //   // ctx.beginPath();
+  //   // ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+  //   // ctx.fill();
+  //   const maxRadius = 35;
 
-    const randomNumber = Math.floor(Math.random() * 4);
-    const randomTrueOrFalse = Math.floor(Math.random() * 2);
+  //   const randomNumber = Math.floor(Math.random() * 4);
+  //   const randomTrueOrFalse = Math.floor(Math.random() * 2);
 
-    this.xCoordinate = xCoordinate;
-    this.yCoordinate = yCoordinate;
-    this.radius = radius;
-    this.color = colorArray[randomNumber];
+  //   this.xCoordinate = xCoordinate;
+  //   this.yCoordinate = yCoordinate;
+  //   this.radius = radius;
+  //   this.color = colorArray[randomNumber];
 
-    if (randomTrueOrFalse == 1) {
-      this.xVelocity = -Math.random() * 1;
-    } else {
-      this.xVelocity = Math.random() * 1;
-    }
+  //   if (randomTrueOrFalse == 1) {
+  //     this.xVelocity = -Math.random() * 1;
+  //   } else {
+  //     this.xVelocity = Math.random() * 1;
+  //   }
 
-    if (randomTrueOrFalse == 1) {
-      this.yVelocity = -Math.random() * 1;
-    } else {
-      this.yVelocity = Math.random() * 1;
-    }
+  //   if (randomTrueOrFalse == 1) {
+  //     this.yVelocity = -Math.random() * 1;
+  //   } else {
+  //     this.yVelocity = Math.random() * 1;
+  //   }
 
-    // As distance gets closer to 0, increase radius
+  //   // As distance gets closer to 0, increase radius
 
-    this.update = function () {
-      this.xCoordinate += this.xVelocity;
-      const xDistance = mouseX - this.xCoordinate;
-      const yDistance = mouseY - this.yCoordinate;
-      const originalRadius = radius;
-      this.yCoordinate += this.yVelocity;
+  //   this.update = function () {
+  //     this.xCoordinate += this.xVelocity;
+  //     const xDistance = mouseX - this.xCoordinate;
+  //     const yDistance = mouseY - this.yCoordinate;
+  //     const originalRadius = radius;
+  //     this.yCoordinate += this.yVelocity;
 
-      // Movement Functions
-      if (
-        this.xCoordinate + this.radius > ctx.canvas.width ||
-        this.xCoordinate - this.radius < 0
-      ) {
-        this.xVelocity = -this.xVelocity;
-      }
-      if (
-        this.yCoordinate + this.radius > ctx.canvas.height ||
-        this.yCoordinate - this.radius < 0
-      ) {
-        this.yVelocity = -this.yVelocity;
-      }
+  //     // Movement Functions
+  //     if (
+  //       this.xCoordinate + this.radius > ctx.canvas.width ||
+  //       this.xCoordinate - this.radius < 0
+  //     ) {
+  //       this.xVelocity = -this.xVelocity;
+  //     }
+  //     if (
+  //       this.yCoordinate + this.radius > ctx.canvas.height ||
+  //       this.yCoordinate - this.radius < 0
+  //     ) {
+  //       this.yVelocity = -this.yVelocity;
+  //     }
 
-      // Radius Decrease Functions
-      // When distance between circle center and mouse on horizontal axis is less than 50, increase radius until it is equal to 35
-      if (
-        xDistance < 50 &&
-        xDistance > -50 &&
-        this.radius < maxRadius &&
-        yDistance < 50 &&
-        yDistance > -50
-      ) {
-        this.radius += 2;
-      } else if (
-        (xDistance >= 50 && originalRadius < this.radius) ||
-        (xDistance <= -50 && originalRadius < this.radius) ||
-        (yDistance >= 50 && originalRadius < this.radius) ||
-        (yDistance <= -50 && originalRadius < this.radius)
-      ) {
-        this.radius -= 2;
-      }
+  //     // Radius Decrease Functions
+  //     // When distance between circle center and mouse on horizontal axis is less than 50, increase radius until it is equal to 35
+  //     if (
+  //       xDistance < 50 &&
+  //       xDistance > -50 &&
+  //       this.radius < maxRadius &&
+  //       yDistance < 50 &&
+  //       yDistance > -50
+  //     ) {
+  //       this.radius += 2;
+  //     } else if (
+  //       (xDistance >= 50 && originalRadius < this.radius) ||
+  //       (xDistance <= -50 && originalRadius < this.radius) ||
+  //       (yDistance >= 50 && originalRadius < this.radius) ||
+  //       (yDistance <= -50 && originalRadius < this.radius)
+  //     ) {
+  //       this.radius -= 2;
+  //     }
 
-      this.draw();
-    };
+  //     this.draw();
+  //   };
 
-    this.draw = function () {
-      ctx.beginPath();
-      ctx.arc(this.xCoordinate, this.yCoordinate, Math.abs(this.radius), 0, Math.PI * 2);
-      ctx.fillStyle = this.color;
-      ctx.fill();
-    };
-  }
+  //   this.draw = function () {
+  //     ctx.beginPath();
+  //     ctx.arc(this.xCoordinate, this.yCoordinate, Math.abs(this.radius), 0, Math.PI * 2);
+  //     ctx.fillStyle = this.color;
+  //     ctx.fill();
+  //   };
+  // }
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    let frameCount = 0;
-    let animationFrameId;
-    let radians = 0;
-    let alpha = 1;
-    let particles = [];
-    let mouseX;
-    let mouseY;
-    canvas.height = window.innerHeight / 2;
-    canvas.width = window.innerWidth;
+  // useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   const ctx = canvas.getContext('2d');
+  //   let frameCount = 0;
+  //   let animationFrameId;
+  //   let radians = 0;
+  //   let alpha = 1;
+  //   let particles = [];
+  //   let mouseX;
+  //   let mouseY;
+  //   canvas.height = window.innerHeight / 2;
+  //   canvas.width = window.innerWidth;
 
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
+  //   const canvasWidth = canvas.width;
+  //   const canvasHeight = canvas.height;
 
-    const maxRadius = 35;
+  //   const maxRadius = 35;
 
-    canvas.onmousemove = function (e) {
-      mouseX = e.clientX;
-      mouseY = e.clientY;
-    };
+  //   canvas.onmousemove = function (e) {
+  //     mouseX = e.clientX;
+  //     mouseY = e.clientY;
+  //   };
 
-    window.addEventListener('resize', function () {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    });
+  //   window.addEventListener('resize', function () {
+  //     canvas.width = window.innerWidth;
+  //     canvas.height = window.innerHeight;
+  //   });
 
-    const render = () => {
-      animationFrameId = window.requestAnimationFrame(render);
-      const myCircle = new Circle(30, 80, 10, ctx, mouseX, mouseY);
-      let circleArray = [];
+  //   const render = () => {
+  //     animationFrameId = window.requestAnimationFrame(render);
+  //     const myCircle = new Circle(30, 80, 10, ctx, mouseX, mouseY);
+  //     let circleArray = [];
 
-      for (let i = 0; i < 20; i++) {
-        const randomXCoordinate = Math.random() * ctx.canvas.width;
-        const randomYCoordinate = Math.random() * ctx.canvas.height;
-        const randomRadius = Math.random() * 5;
-        circleArray.push(
-          new Circle(
-            randomXCoordinate,
-            randomYCoordinate,
-            randomRadius,
-            ctx,
-            mouseX,
-            mouseY,
-          ),
-        );
-      }
+  //     for (let i = 0; i < 20; i++) {
+  //       const randomXCoordinate = Math.random() * ctx.canvas.width;
+  //       const randomYCoordinate = Math.random() * ctx.canvas.height;
+  //       const randomRadius = Math.random() * 5;
+  //       circleArray.push(
+  //         new Circle(
+  //           randomXCoordinate,
+  //           randomYCoordinate,
+  //           randomRadius,
+  //           ctx,
+  //           mouseX,
+  //           mouseY,
+  //         ),
+  //       );
+  //     }
 
-      function updateAll() {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        myCircle.update();
-        for (let i = 0; i < circleArray.length; i++) {
-          circleArray[i].update();
-        }
-        window.requestAnimationFrame(updateAll);
-      }
+  //     function updateAll() {
+  //       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  //       myCircle.update();
+  //       for (let i = 0; i < circleArray.length; i++) {
+  //         circleArray[i].update();
+  //       }
+  //       window.requestAnimationFrame(updateAll);
+  //     }
 
-      updateAll();
-    };
-    render();
+  //     updateAll();
+  //   };
+  //   render();
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
-  }, [Circle]);
+  //   return () => {
+  //     window.cancelAnimationFrame(animationFrameId);
+  //   };
+  // }, [Circle]);
 
   return (
     <>
@@ -206,7 +206,7 @@ const Home: NextPage = (props: any) => {
         first={
           <AppLayout.First>
             <div className="absolute -z-10">
-              <canvas ref={canvasRef} {...props} />
+              {/* <canvas ref={canvasRef} {...props} /> */}
             </div>
             <Main />
           </AppLayout.First>
